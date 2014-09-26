4f68e269d8dc8e64e7f497eb5bff5ff39baf1a19
Chungsub Kim <chungsub.kim@purpleworks.co.kr>
2014-09-26 17:45:38 +0900

    update

--- http.go
+++ http.go
@@ -10,7 +10,7 @@ import (
 func main() {
     port := os.Getenv("PORT")
     if port == "" {
-        port = "8080"
+        port = "80"
     }
 
     fmt.Fprintf(os.Stdout, "Listening on :%s\n", port)
